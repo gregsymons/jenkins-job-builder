1514df0d1014cfa8aa4be44eba2c411dcdf24d8a
Gil Forcada <gforcada@gnome.org>
2015-02-15 15:18:50 +0100

    Add custom history plugin

--- jenkins_jobs/modules/publishers.py
+++ jenkins_jobs/modules/publishers.py
@@ -4330,6 +4330,36 @@ def display_upstream_changes(parser, xml_parent, data):
         'DisplayUpstreamChangesRecorder')
 
 
+def custom_history(parser, xml_parent, data):
+    """yaml: custom-history
+
+    Show a custom file for the history.
+    Requires the Jenkins `Custom History.
+    <https://wiki.jenkins-ci.org/display/JENKINS/Custom+History>`_
+
+    :arg str file: filename that will be used to show the history form
+    :arg bool exit-on-fail: whether it exit if it fails (default False)
+
+    Example::
+
+      publishers:
+        - custom-history:
+            file: CHANGES.log
+    """
+    history_data = XML.SubElement(
+        xml_parent,
+        'org.jenkinsci.plugins.custom__history.SaveHistory'
+    )
+
+    data_property = str(data.get('file'))
+    XML.SubElement(history_data, 'fname').text = data_property
+
+    data_property = str(data.get('exit-on-fail', False)).lower()
+    XML.SubElement(history_data, 'exitOnFail').text = data_property
+
+    data_property = str(data.get('exit-on-fail', False)).lower()
+    XML.SubElement(history_data, 'exitOnFail').text = data_property
+
 def gatling(parser, xml_parent, data):
     """yaml: gatling
     Publish gatling results as a trend graph
