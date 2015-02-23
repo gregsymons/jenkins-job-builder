2648c304878be2b71957a468fb91a04019a06b58
Gil Forcada <gforcada@gnome.org>
2015-02-23 23:47:14 +0100

    New module: definitions
    As part of the new Jenkins workflow plugin[1] a new root element
    is needed to hold the pipeline configuration: definitions.
    
    Right not only supports externally defined pipelines.
    
    [1] https://wiki.jenkins-ci.org/display/JENKINS/Workflow+Plugin

---
+++ jenkins_jobs/modules/definitions.py
@@ -0,0 +1,44 @@
+# Copyright 2015 Gil Forcada Codinachs <gforcada@gnome.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+"""
+LATER
+"""
+
+
+import xml.etree.ElementTree as XML
+import jenkins_jobs.modules.base
+
+
+def scm(parser, xml_parent, data):
+    from jenkins_jobs.modules.scm import git
+    git(parser, xml_parent, data)
+
+
+def script(parser, xml_parent, data):
+    XML.SubElement(xml_parent, 'scriptPath').text = data.get('filename')
+
+
+class Definitions(jenkins_jobs.modules.base.Base):
+    sequence = 70
+
+    component_type = 'definition'
+    component_list_type = 'definitions'
+
+    def gen_xml(self, parser, xml_parent, data):
+        publishers = XML.SubElement(xml_parent, 'definition')
+
+        for action in data.get('definitions', []):
+            self.registry.dispatch('definition', parser, publishers, action)
